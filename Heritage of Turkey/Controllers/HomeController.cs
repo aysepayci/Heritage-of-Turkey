b22a25646951e22a2cb784d939f187a97853d415
aysepayci <aysepayci0@gmail.com>
2026-05-08 17:55:24 +0300

    Initial project files added

---
+++ Heritage of Turkey/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Heritage_of_Turkey.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Heritage_of_Turkey.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
